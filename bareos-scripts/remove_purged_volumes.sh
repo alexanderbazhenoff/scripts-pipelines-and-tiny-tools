7c80e43b6bdeeae394c671f8b29e0bac49df541a
Alexander Bazhenov <bazhenoff.alexander@gmail.com>
2019-01-12 16:21:30 +0300

    Create remove_purged_volumes.sh

---
+++ bareos-scripts/remove_purged_volumes.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# ----------------------------------------------------------------------- #
+# Removes all voluems physically from the disk which is in 'purged' state #
+# ----------------------------------------------------------------------- #
+
+for f in `echo "list volume" | bconsole | grep Purged | cut -d ' ' -f6`; do
+  echo "delete volume=$f yes" | bconsole;
+  rm -rf /mnt/nas/$f;
+done
+
+exit 0
