0d8973a042b2a0bcd5189310c0e15a62efa03981
alexanderbazhenoff <bazhenoff.alexander@gmail.com>
2023-02-22 00:58:01 +0300

    license url fixed

--- calculate_and_check_md5/calculate_md5.sh
+++ calculate_and_check_md5/calculate_md5.sh
@@ -8,8 +8,8 @@
 ###          script at your own risk.
 ### -----------------------------------------------------------------------------------------------
 ### This Source Code Form is subject to the terms of the MIT License. If a copy of the MPL was not
-### distributed with this file, You can obtain one at https://github.com/aws/mit-0
-
+### distributed with this file, You can obtain one at:
+### https://github.com/alexanderbazhenoff/data-scripts/blob/master/LICENSE
 
 SOURCE_PATH="/mnt/backup/"
 
