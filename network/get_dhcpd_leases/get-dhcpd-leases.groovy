2951f547503b0cfa5f2b44f7f6be3a316ccb69ad
alexanderbazhenoff <bazhenoff.alexander@gmail.com>
2024-07-03 23:00:54 +0300

    wip

--- network/get_dhcpd_leases/get-dhcpd-leases.groovy
+++ network/get_dhcpd_leases/get-dhcpd-leases.groovy
@@ -14,17 +14,17 @@
 // Start jenkins node where git clone (and optional all required keys for ssh cloning)
 final String StartNode = 'master'
 
-// Jenkins node with isc-dhcp-server to execute on
-final String ExecutionNode = 'dhcpd-server.domain'
+// Jenkins node with isc-dhcp-server to execute on, e.g.: dhcpd-server.domain
+final String ExecutionNode = 'gate'
 
 // Repo URL of 'get_dhcpd_leases.py' script, e.g: 'git@github.com:alexanderbazhenoff/various-scripts.git'
-final String GitProjectUrlOfTheScript = 'https://github.com/alexanderbazhenoff/various-scripts.git'
+final String GitProjectUrlOfTheScript = 'https://github.com/alexanderbazhenoff/scripts-pipelines-and-tiny-tools.git'
 
 // Full path inside the repo to run 'get_dhcpd_leases.py' script
 final String ScriptPathInsideTheRepo = 'network/get_dhcpd_leases/get_dhcpd_leases.py'
 
 // Repo branch
-final String GitScriptBranch = 'master'
+final String GitScriptBranch = 'restructured'
 
 // Credentials to access repo (for ssh cloning), e.g. 'a222b01a-230b-1234-1a12345678b9'
 final String GitCredentials = ''
